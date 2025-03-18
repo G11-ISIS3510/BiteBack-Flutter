5a0d690a8cb6278e7fdb773b22361e9ebe9768d1
MrCheesyBurgerU <n.rozo@uniandes.edu.co>
2025-03-18 17:29:56 -0500

    Home View Finished with Functionalities

--- lib/viewmodels/home_viewmodel.dart
+++ lib/viewmodels/home_viewmodel.dart
@@ -10,88 +10,85 @@ import '../services/location_service.dart';
 
 class HomeViewModel extends ChangeNotifier {
 
-  // Dependencias a repositorios
+  // Inyección de repositorios para manejo de la base de datos
   final BusinessRepository _businessRepository = BusinessRepository();
   final ProductsRepository _productsRepository = ProductsRepository();
 
-  // Variables de clase con valores iniciales
+  // Variables de clase y de estado
   String _userName = "Usuario";
   UserLocation? _location;
   String _address = "Ubicación no disponible";
   List<Business> _allRestaurants = [];
-  List<Business> _filteredrestaurants = [];
   List<Product> _allProducts = [];
   List<Product> _nearbyProducts = [];
+  List<Product> _filteredProducts = [];
   Set<String> _categories = {};
   String _selectedCategory = "";
+  String _searchQuery = "";
 
-
-  // Getters para exponer los valores
+  // Getters para obtener los atributos
   String get userName => _userName;
   UserLocation? get location => _location;
   String get address => _address;
   List<Business> get allRestaurants => _allRestaurants;
-  List<Business> get filteredRestaurants => _filteredrestaurants;
   Set<String> get categories => _categories;
   String get selectedCategory => _selectedCategory;
   List<Product> get allProducts => _allProducts;
   List<Product> get nearbyProducts => _nearbyProducts;
+  List<Product> get filteredProducts => _filteredProducts; 
+  String get searchQuery => _searchQuery; 
 
-  // Carga los datos del usuario y su ubicación al instanciarse
-  // Tambien se cargan las categorias
+  // Carga de los datos del usuario y categorias
   HomeViewModel() {
     _loadUserData();
     _loadCategories();
   }
 
-  // Método para obtener el nombre y ubicación del usuario autenticado
+  // Método que carga el nombre y ubicación del usuario
+  // Támbien carga los restaurantes, productos y los asocia
   Future<void> _loadUserData() async {
-    // Obtener usuario autenticado
+
     User? user = FirebaseAuth.instance.currentUser;
     if (user != null) {
       _userName = user.displayName ?? user.email ?? "Usuario";
     }
 
-    // Obtener la ubicación usando LocationService
+    // 
     Position? position = await LocationService.getCurrentPosition();
     if (position != null) {
       _location = UserLocation(latitude: position.latitude, longitude: position.longitude);
       _address = await LocationService.getAddressFromCoordinates(position);
-    } 
-    else {
+    } else {
       _address = "No se pudo obtener la ubicación.";
     }
 
-    // Notificar cambios a la vista
     notifyListeners();
 
-    // Carga de los restaurantes y productos
+    // Espera a la carga de los recursos
     await Future.wait([
       _loadRestaurants(),
       _loadProducts(),
     ]);
 
-    // Vincular productos a restaurantes
+    // Linkea y carga productos cercanos
     _linkProductsToRestaurants();
-
-    // Carga de productos más cercanos
     _loadNearbyProducts();
   }
 
-  // Método para obtener todos los negocios de tipo restaurante
+  // Método para cargar los restaurantes
   Future<void> _loadRestaurants() async {
     _allRestaurants = await _businessRepository.getRestaurants();
-    _filteredrestaurants = _allRestaurants;
-    notifyListeners(); 
-  }
-
-  // Método para obtener todos los productos ofertados
-  Future<void> _loadProducts() async {
-    _allProducts = await _productsRepository.getProducts();
     notifyListeners();
   }
 
-  // Método para linkear cada producto a su restaurante correspondeinte
+  // Método para cargar los productos
+  Future<void> _loadProducts() async {
+    _allProducts = await _productsRepository.getProducts();
+    _filteredProducts = _allProducts;
+    notifyListeners();
+  }
+
+  // Método para asociar productos con restaurantes
   void _linkProductsToRestaurants() {
     for (var restaurant in _allRestaurants) {
       restaurant.products = _allProducts.where((product) => product.businessId == restaurant.id).toList();
@@ -99,15 +96,13 @@ class HomeViewModel extends ChangeNotifier {
     notifyListeners();
   }
 
-  // Método para cargar los restaurantes más cercanos a la ubicación del usuario
+  // Método para cargar los productos cercanos
   void _loadNearbyProducts() {
     if (_location == null) return;
 
-    // Distancia máxima de 5 km
     const double maxDistance = 5000; 
     List<Product> nearbyProducts = [];
 
-    // Se revisa que restaurantes cumplen con la cota de distancia
     for (var restaurant in _allRestaurants) {
       double distance = Geolocator.distanceBetween(
         _location!.latitude, 
@@ -125,29 +120,35 @@ class HomeViewModel extends ChangeNotifier {
     notifyListeners();
   }
 
-  // Método para filtrar restaurantes por un nombre
-  void filterRestaurants(String query) {
-    if (query.isEmpty) {
-      _filteredrestaurants = allRestaurants;
-    } 
-    // Se ejecuta la consulta en caso de que exista
-    else {
-      _filteredrestaurants = allRestaurants.where((restaurant) {
-        return restaurant.name.toLowerCase().contains(query.toLowerCase());
-      }).toList();
-    }
-    notifyListeners();
-  }
-
-  // Método para obtener las categorias unicas de comida
+  // Método para cargar las categorias
   Future<void> _loadCategories() async {
     _categories = await _productsRepository.getUniqueCategories();
-    notifyListeners(); 
-  }
-
-  // Método para actualizar la categoría seleccionada en la vista
-  void setSelectedCategory(String category) {
-    _selectedCategory = category;
     notifyListeners();
   }
+
+  // Método para manejar la selección de categorias
+  void setSelectedCategory(String category) {
+    if (_selectedCategory == category) {
+      _selectedCategory = ""; 
+    } else {
+      _selectedCategory = category;
+    }
+    filterProducts(_searchQuery); 
+  }
+
+  // Método para hacer el filtrado de productos
+  void filterProducts(String query) {
+
+    _searchQuery = query; 
+
+    _filteredProducts = _allProducts.where((product) {
+      bool matchesQuery = query.isEmpty || product.name.toLowerCase().contains(query.toLowerCase());
+      bool matchesCategory = _selectedCategory.isEmpty || product.category == _selectedCategory;
+      return matchesQuery && matchesCategory;
+    }).toList();
+
+    notifyListeners(); 
+  }
 }
+
+
--- lib/views/home_screen_view.dart
+++ lib/views/home_screen_view.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: use_key_in_widget_constructors, deprecated_member_use, unnecessary_brace_in_string_interps
 
+import 'package:biteback/widgets/custom_bottom_navbar.dart';
 import 'package:biteback/widgets/explore_banner.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -14,7 +15,7 @@ class HomeScreen extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (_) => HomeViewModel(),
       child: Scaffold(
-        bottomNavigationBar: _buildBottomNavigationBar(),
+        bottomNavigationBar: CustomBottomNavBar(),
         body: SafeArea(
           child: SingleChildScrollView(
             padding: EdgeInsets.all(16.0),
@@ -22,16 +23,12 @@ class HomeScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 _buildHeader(),
-                SearchBarWithVoice(
-                  onSearch: (query) {
-                    Provider.of<HomeViewModel>(context, listen: false)
-                        .filterRestaurants(query);
-                  },
-                ),
-                DiscountBanner(), // Aquí usamos el nuevo banner
+                SearchBarWithVoice(),
+                DiscountBanner(),
                 ExploreBanners(),
                 _buildCategories(),
-                _buildNearbyProducts()
+                _buildAllProducts(), // Nueva función que ahora maneja la búsqueda
+                _buildNearbyProducts(),
               ],
             ),
           ),
@@ -107,7 +104,7 @@ Widget _categoryCard(String title, String selectedCategory, Function(String) onS
       margin: EdgeInsets.symmetric(horizontal: 8, vertical: 6),
       padding: EdgeInsets.all(12),
       decoration: BoxDecoration(
-        color: isSelected ? Colors.orange : Colors.white, // Fondo cambia al seleccionar
+        color: isSelected ? Colors.orange : Colors.white,
         borderRadius: BorderRadius.circular(20),
         boxShadow: [
           BoxShadow(
@@ -121,7 +118,7 @@ Widget _categoryCard(String title, String selectedCategory, Function(String) onS
         title,
         style: TextStyle(
           fontWeight: FontWeight.bold,
-          color: isSelected ? Colors.white : Colors.black, // Texto cambia al seleccionar
+          color: isSelected ? Colors.white : Colors.black,
         ),
       ),
     ),
@@ -161,70 +158,100 @@ Widget _categoryCard(String title, String selectedCategory, Function(String) onS
   );
 }
 
+Widget _buildAllProducts() {
+  return Consumer<HomeViewModel>(
+    builder: (context, viewModel, child) {
+      final List<Product> productsToShow = viewModel.filteredProducts;
+
+      if (productsToShow.isEmpty) {
+        return Center(child: Text("No se encontraron productos."));
+      }
+
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 4, vertical: 8),
+            child: Text(
+              viewModel.searchQuery.isNotEmpty || viewModel.selectedCategory.isNotEmpty
+                  ? "Resultados de la búsqueda"
+                  : "Todos los productos",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+          SizedBox(height: 8),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: productsToShow.map((product) {
+                return _productCard(product);
+              }).toList(),
+            ),
+          ),
+        ],
+      );
+    },
+  );
+}
 
 Widget _productCard(Product product) {
-  return Container(
-    margin: EdgeInsets.only(right: 10),
-    width: 160,
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Stack(
-          children: [
-            Image.network(product.image, height: 100, width: 160, fit: BoxFit.cover),
-            if (product.discount > 0) // Solo mostrar si hay descuento
-              Positioned(
-                top: 8,
-                left: 8,
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 6, vertical: 2),
-                  color: Colors.red,
-                  child: Text(
-                    "${product.discount.toInt()}% off",
-                    style: TextStyle(color: Colors.white),
+  return ClipRRect(
+    borderRadius: BorderRadius.circular(20), // Aplica el recorte a toda la tarjeta
+    child: Container(
+      margin: EdgeInsets.only(right: 10),
+      width: 160,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.white, // Mantiene el fondo blanco sin sombra
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20)), 
+                child: Image.network(product.image, height: 100, width: 160, fit: BoxFit.cover),
+              ),
+              if (product.discount > 0)
+                Positioned(
+                  top: 8,
+                  left: 8,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 6, vertical: 2),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text(
+                      "${product.discount.toInt()}% off",
+                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
                   ),
                 ),
-              ),
-          ],
-        ),
-        SizedBox(height: 5),
-        Text(product.name, style: TextStyle(fontWeight: FontWeight.bold)),
-        Text("\$${product.price.toStringAsFixed(2)}", 
-          style: TextStyle(color: Colors.orange, fontWeight: FontWeight.bold)),
-      ],
+            ],
+          ),
+          SizedBox(height: 5),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(product.name, style: TextStyle(fontWeight: FontWeight.bold)),
+                Text("\$${product.price.toStringAsFixed(2)}",
+                    style: TextStyle(color: Colors.orange, fontWeight: FontWeight.bold)),
+              ],
+            ),
+          ),
+        ],
+      ),
     ),
   );
 }
 
 
 
-  Widget _buildBottomNavigationBar() {
-    return Container(
-      margin: EdgeInsets.all(8.0),
-      decoration: BoxDecoration(
-        color: Colors.grey[300],
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.1),
-            blurRadius: 10,
-            offset: Offset(0, -2),
-          ),
-        ],
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20),
-        child: BottomNavigationBar(
-          backgroundColor: Colors.grey[300],
-          type: BottomNavigationBarType.fixed,
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(icon: Icon(Icons.category), label: "Categories"),
-            BottomNavigationBarItem(icon: Icon(Icons.shopping_cart), label: "Cart"),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
-          ],
-        ),
-      ),
-    );
-  }
+
+
+
+
 }
